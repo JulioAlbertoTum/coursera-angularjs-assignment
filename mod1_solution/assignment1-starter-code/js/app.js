55310ffb12cfd256dad5feaf03fd46d9e7b5afe5
Julio Alberto T <jultum88@gmail.com>
2016-09-19 04:03:52 -0400

    Add folder mod1_solution

---
+++ mod1_solution/assignment1-starter-code/js/app.js
@@ -0,0 +1,42 @@
+(function () {
+'use strict';
+
+angular.module('LunchApp', [])
+.controller('LunchController', LunchController);
+
+LunchController.$inject = ['$scope'];
+function LunchController($scope) {
+  // $scope.message ="";
+  $scope.stateMessage = "s-default"
+  $scope.count = 0;
+    function countItems () {
+      var listItems = [];
+      if($scope.items != undefined && $scope.items != ""){
+        listItems = $scope.items.split(',').map(function(item){
+          return item.trim();
+        }).filter(function(v){
+          return v !== ''
+        });
+      }
+      // console.log(listItems);
+      return listItems.length;
+    };
+
+  $scope.showMessage = function(){
+    $scope.count = countItems();
+    if ( $scope.count == 0){
+      $scope.message = "Please enter data first";
+      $scope.stateMessage = "s-default";
+    }else if($scope.count!=0 && $scope.count <= 3){
+      $scope.message = "Enjoy!";
+      $scope.stateMessage = "s-success";
+    }else if($scope.count > 3){
+      $scope.message = "Too much!";
+      $scope.stateMessage = "s-danger";
+    }
+  };
+
+
+}
+
+})();
